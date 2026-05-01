de94dcb5c970e79db868def2523e0b0cd27b5802
Rudolf Klusal <klusik@klusik.cz>
2026-05-01 15:43:06 +0200

    Version 0.37

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-const CMS_VERSION = '0.36';
+const CMS_VERSION = '0.37';
 const CMS_GITHUB_REPOSITORY = 'klusik/PHP_gallery';
 const CMS_UPDATE_BRANCHES = ['main', 'master'];
 
